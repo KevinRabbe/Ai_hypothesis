87b42f465be3822c908a219481b821d275188627
KevinRabbe <rabbekevin@icloud.com>
2026-07-29 20:38:58 +0200

    Add fixed Gate3 v1 robustness seed1 wrapper

---
+++ scripts/run_gate3_v1_robustness_seed1.ps1
@@ -0,0 +1,15 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [switch]$IdleMachineAttested,
+
+    [string]$OutputRoot = "F:\gate3_v1_sparse_active_reserve_robustness_seed_1"
+)
+
+& (Join-Path $PSScriptRoot "run_gate3_v1_robustness_common.ps1") `
+    -TrainingSeed 1 `
+    -IdleMachineAttested:$IdleMachineAttested `
+    -OutputRoot $OutputRoot
+
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
